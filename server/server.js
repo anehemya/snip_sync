70b5dc9dbdfa3bdaa67ef32cba69322c03dc885d
Akiva <akyeshurun@gmail.com>
2025-03-23 12:32:00 -0400

    modified server file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
-process.env.NODE_OPTIONS = '--openssl-legacy-provider';
-
 const express = require('express');
 const cors = require('cors');
 const appointmentRoutes = require('./routes/appointmentRoutes');
 const { google } = require('googleapis');
 const credentials = require('./config/config').google;
+
+// Add this for debugging
+console.log('Starting server with credentials:', {
+  type: credentials.type,
+  project_id: credentials.project_id,
+  client_email: credentials.client_email
+  // Don't log private key or sensitive data
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
