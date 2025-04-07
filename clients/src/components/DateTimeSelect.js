1e493ccabacfbf3434f4762160bf759685174a41
Akiva <akyeshurun@gmail.com>
2025-04-07 15:19:32 -0400

    updated UI 2

--- clients/src/components/DateTimeSelect.js
+++ clients/src/components/DateTimeSelect.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import DatePicker from 'react-datepicker';
 import { getAvailability } from '../utils/api';
 import "react-datepicker/dist/react-datepicker.css";
